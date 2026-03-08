e3115f8a2239f4c009969f8cc274b44f17f61219
aljambul <jambul9827@gmail.com>
2026-03-09 00:28:54 +0500

    added: connection to database(postgres and redis) and rmq

--- backend/services/pkg/database/postgres.go
+++ backend/services/pkg/database/postgres.go
@@ -1,1 +1,31 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"online_taxi/services/pkg/models"
+)
+
+func ConnectToDB(cfg *models.Database) (*pgxpool.Pool, error) {
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable&pool_max_conns=10",
+		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Name)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	pool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка парсинга конфига БД: %w", err)
+	}
+
+	if err = pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("база данных недоступна (ping failed): %w", err)
+	}
+
+	fmt.Println("✅ Успешное подключение к PostgreSQL (pgxpool)!")
+
+	return pool, nil
+}
---
+++ backend/services/pkg/models/db.go
@@ -0,0 +1,11 @@
+package models
+
+type Database struct {
+	User      string
+	Password  string
+	Host      string
+	Name      string
+	Port      string
+	RedisPort string
+	RedisHost string
+}
