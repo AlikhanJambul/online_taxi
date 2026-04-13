31ba7aba1eb26cd952123e33aba54f698f4b8e49
aljambul <jambul9827@gmail.com>
2026-04-13 23:38:26 +0500

    add functions FindAndNotifyDrivers and GetFCMTokens

--- backend/services/trip-service/internal/domain/repository.go
+++ backend/services/trip-service/internal/domain/repository.go
@@ -8,4 +8,5 @@ type Repository interface {
 	CreateTrip(ctx context.Context, trip *Trip) error
 	AcceptTrip(ctx context.Context, tripID string, driverID string) (*Trip, error)
 	GetTrip(ctx context.Context, tripID string) (*Trip, error)
+	GetFCMTokens(ctx context.Context, driverIDs []string) ([]string, error)
 }
